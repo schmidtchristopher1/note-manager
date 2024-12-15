ecce02ca4ac0a465252809b01bd51cd3b4f9f23b
Martin Mirda <martin.mirda@kosickaakademia.sk>
2024-12-15 20:57:45 +0100

    Refactor notes list to dynamically display notes from API; remove static card entries

--- src/app/pages/notes-list/notes-list.component.ts
+++ src/app/pages/notes-list/notes-list.component.ts
@@ -1,14 +1,26 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NotesService, Note } from '../../services/notes.service';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-notes-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HttpClientModule],
   templateUrl: './notes-list.component.html',
   styleUrls: ['./notes-list.component.css'],
 })
-export class NotesListComponent  {
+export class NotesListComponent implements OnInit {
+
+  notes: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any[]>('http://localhost:3000/notes').subscribe(data => {
+      this.notes = data;
+    });
+  }
+
+
 
 }
