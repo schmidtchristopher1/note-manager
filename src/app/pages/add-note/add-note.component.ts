dd5da3a10caa294b0c9be57fefa7f76da953bbf2
Martin Mirda <martin.mirda@kosickaakademia.sk>
2024-12-15 21:09:47 +0100

    Add functionality to create and submit new notes with dynamic form handling

--- src/app/pages/add-note/add-note.component.ts
+++ src/app/pages/add-note/add-note.component.ts
@@ -1,13 +1,27 @@
 import { Component } from '@angular/core';
-
-
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-note',
   standalone: true,
+  imports: [HttpClientModule, FormsModule],
   templateUrl: './add-note.component.html',
   styleUrls: ['./add-note.component.css'],
 })
 export class AddNoteComponent {
+  note = {
+    title: '',
+    content: ''
+  };
 
+  constructor(private http: HttpClient) {}
+
+  addNote(): void {
+    this.http.post('http://localhost:3000/notes', this.note).subscribe(response => {
+      console.log('Note added', response);
+     
+      this.note = { title: '', content: '' };
+    });
+  }
 }
