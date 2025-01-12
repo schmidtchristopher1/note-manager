c81dd82ad646bc6ed6116de1364327e647d33596
Martin Mirda <martin.mirda@kosickaakademia.sk>
2025-01-12 12:13:47 +0100

    Add login component with routing and basic styling

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { AddNoteComponent } from './pages/add-note/add-note.component';
+import { LoginComponent } from './pages/login/login.component';
 
 
 export const routes: Routes = [
-    { path: '', redirectTo: '', pathMatch: 'full' },
+    {path: '', redirectTo: 'login', pathMatch: 'full'},
+    {path: 'login', component: LoginComponent},
     { path: 'notes', component: NotesListComponent },
     { path: 'add-note', component: AddNoteComponent },
 ];
