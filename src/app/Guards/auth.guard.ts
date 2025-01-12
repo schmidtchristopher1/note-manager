41ebd68d5cfe17bbcf8aaab331eab2322c889a70
Martin Mirda <martin.mirda@kosickaakademia.sk>
2025-01-12 18:16:18 +0100

    Add authentication guard to protect routes from unauthorized access

--- src/app/Guards/auth.guard.ts
+++ src/app/Guards/auth.guard.ts
@@ -1,6 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
+import { inject } from '@angular/core';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(AuthService)
+  const token = authService.getToken();
+
+  if (!token) {
+    return false;
+    const router = inject(Router);
+    router.navigate(['/login']);
+  }
   
   return true;
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,14 @@ import { Routes } from '@angular/router';
 import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { AddNoteComponent } from './pages/add-note/add-note.component';
 import { LoginComponent } from './pages/login/login.component';
+import { authGuard } from './Guards/auth.guard';
 
 
 export const routes: Routes = [
     {path: '', redirectTo: 'login', pathMatch: 'full'},
     {path: 'login', component: LoginComponent},
-    { path: 'notes', component: NotesListComponent },
-    { path: 'add-note', component: AddNoteComponent },
+    { path: 'notes', component: NotesListComponent, canActivate: [authGuard] },
+    { path: 'add-note', component: AddNoteComponent, canActivate: [authGuard] },
 ];
 
 
