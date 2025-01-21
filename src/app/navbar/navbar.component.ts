3800d810cce7bb3a9533e67bebf5fd115515795b
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-01-21 11:21:39 +0100

    Add authentication check in navbar component to redirect users based on token presence

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
 
 @Component({
@@ -7,13 +8,19 @@ import { Router, RouterLink, RouterLinkActive } from '@angular/router';
   standalone: true,
   imports: [RouterLink, RouterLinkActive],
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css'], 
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent {
 
   router = inject(Router);
-  
+  as = inject(AuthService);
 
-
-
+  constructor() {
+    const token = this.as.getToken();
+    if (token == null) {
+      this.router.navigate(['/login']);
+    } else {
+      this.router.navigate(['/notes']);
+    }
+  }
 }
