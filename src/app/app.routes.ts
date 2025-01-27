c62c2576e688cdab2784a7e620d7a726b14071f2
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-01-27 17:21:15 +0100

    Add registration route to application

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,15 @@ import { NotesListComponent } from './pages/notes-list/notes-list.component';
 import { AddNoteComponent } from './pages/add-note/add-note.component';
 import { LoginComponent } from './pages/login/login.component';
 import { authGuard } from './Guards/auth.guard';
+import { RegisterComponent } from './pages/register/register.component';
 
 
 export const routes: Routes = [
-    {path: '', redirectTo: 'login', pathMatch: 'full'},
-    {path: 'login', component: LoginComponent},
+    { path: '', redirectTo: 'login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
     { path: 'notes', component: NotesListComponent, canActivate: [authGuard] },
     { path: 'add-note', component: AddNoteComponent, canActivate: [authGuard] },
+    { path: 'register', component: RegisterComponent }
 ];
 
 
