0a0029772e3ed3b47ed8e1bcb074b68bc2be6934
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-01-27 17:22:19 +0100

    Add RegisterComponent with form validation and submission logic

---
+++ src/app/pages/register/register.component.ts
@@ -0,0 +1,59 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { HttpClientModule } from '@angular/common/http';
+import { MessageService } from '../../services/message.service';
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [CommonModule, HttpClientModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent {
+  registerForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder,
+    private authService: AuthService,
+    private router: Router,
+    private messageService: MessageService
+  ) {
+    this.registerForm = this.fb.group({
+      username: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+      repeatPassword: ['', [Validators.required]]
+    }, {
+      validators: this.passwordMatchValidator.bind(this)
+    });
+  }
+
+  passwordMatchValidator(group: FormGroup): { [key: string]: any } | null {
+    const password = group.get('password');
+    const repeatPassword = group.get('repeatPassword');
+
+    if (password && repeatPassword && password.value !== repeatPassword.value) {
+      return { 'passwordMismatch': true };
+    }
+    return null;
+  }
+
+  onSubmit() {
+    if (this.registerForm.valid) {
+      const { username, password } = this.registerForm.value;
+      this.authService.register(username, password).subscribe(
+        (response) => {
+          this.messageService.showSuccessMessage('Registration successful!');
+          this.router.navigate(['/login']);
+        },
+        (error) => {
+          const errorMessages = error.error.error;
+          this.messageService.showErrorMessage(errorMessages);
+        }
+      );
+    }
+  }
+}
