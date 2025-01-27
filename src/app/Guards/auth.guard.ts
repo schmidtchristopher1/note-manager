9eab0229aa7d68d4eefe7be5632857aecebea8bf
Christopher Schmidt <christopher.schmidt@kosickaakademia.sk>
2025-01-27 16:04:57 +0100

    remove unnecessary code

--- src/app/Guards/auth.guard.ts
+++ src/app/Guards/auth.guard.ts
@@ -2,15 +2,13 @@ import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from '@angular/core';
 
-export const authGuard: CanActivateFn = (route, state) => {
+export const authGuard: CanActivateFn = () => {
   const authService = inject(AuthService)
   const token = authService.getToken();
 
   if (!token) {
     return false;
-    // const router = inject(Router);
-    // router.navigate(['/login']);
   }
-  
+
   return true;
 };
